8a292233b19d76c89588edea1e01d7f305f446d6
Adelegan-JD <deborahjesudemilade@gmail.com>
2025-10-21 13:15:27 +0100

    added new files

---
+++ Week_11/backend/database.py
@@ -0,0 +1,49 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from pymysql.constants import CLIENT
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+# Create a connection database (the dialect is mysql, the driver is pymysql)
+# db_url = dialect+driver://dbuser;dbpassword;dbhost;dbport;dbname    This is the format of creating a url
+
+db_url = f"mysql+pymysql://{os.getenv("dbuser")}:{os.getenv("dbpassword")}@{os.getenv("dbhost")}:{os.getenv("dbport")}/{os.getenv("dbname")}"
+
+engine = create_engine(db_url)
+
+session = sessionmaker(bind=engine)
+
+db = session()
+
+# query = text("select * from user")
+# user = db.execute(query).fetchall()
+
+# print(user)
+
+create_users = text("""
+
+CREATE TABLE users (
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+email VARCHAR(100) NOT NULL,
+password VARCHAR(25) NOT NULL
+
+);
+
+""")
+
+
+create_courses = text("""
+
+CREAT TABLE courses(
+id INT AUTO_INCEREMENT PRIMARY KEY,
+title VARCHAR(100) NOT NULL,
+level VARCHAR(100) NOT NULL
+);
+""")
+
+db.execute(create_table_query)
+
---
+++ Week_11/backend/simpleapp.py
@@ -0,0 +1,39 @@
+from database import db
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+from sqlalchemy import text
+import os
+import bcrypt
+from dotenv import load_dotenv
+import uvicorn
+
+
+load_dotenv()
+
+app = FastAPI(title='A simple App', version="1.0.0")
+
+
+class Simple(BaseModel):
+    name: str = Field(..., examples= "Debby Ade")
+    email: str = Field(..., examples= "debby@gmail.com")
+    password: str = Field(..., examples= "ade123")
+
+
+@app.post("/signup")
+def SignUp(input: Simple):
+    try:
+        query = text("""
+        INSERT INTO users (name, email, password)
+        VALUES (:name, :email, :password)
+        """)
+
+        salt = bcrypt.gensalt()
+        hashedPassword = bcrypt.hashpw(input.password.encode('utf-8'), salt)
+        print(hashedPassword)
+        db.execute(query, {"name": input.name, "email": input.email, "password": input.password})
+        db.commit()
+
+        return{"Message": "User details uploaded successfully", "data": {"name": input.name}, "email", input.email}
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail = e)
